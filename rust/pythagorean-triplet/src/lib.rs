883f406b4b3ca61b3649cc7cc05d242b187aeb1e
Daniel Ong <danielong1@gmail.com>
2018-12-05 18:36:40 -0800

    add basic algorithm structure

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,5 +1,14 @@
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    unimplemented!("Given the sum {}, return all possible Pythagorean triplets, which produce the said sum, or an empty HashSet if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c", sum);
+    let mut set: HashSet<[u32; 3]>;
+    for a in 2..sum {
+	for b in a..(sum - a) {
+	    let c: u32 = sum - a - b;
+	    if a.pow(2) + b.pow(2) == c.pow(2) {
+		set.insert([a, b, c]);
+	    }
+	}
+    }
+    return set;
 }
